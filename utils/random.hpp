cfa3679b002615b7f3e6c366af2f3f76dd55fb5d
Pachicobue <tigerssho@gmail.com>
2019-08-24 14:24:56 +0900

    リファクタリング

--- utils/random.hpp
+++ utils/random.hpp
@@ -1,30 +1,31 @@
 #pragma once
 #include <algorithm>
 #include <random>
+
 #include "types.hpp"
 namespace libtest {
-class Rng
+class rng
 {
 public:
-    Rng() : Rng(std::random_device{}()) {}
-    Rng(const u64 seed) : mt{seed} {}
+    rng() : rng(std::random_device{}()) {}
+    rng(const u64 seed) : mt{seed} {}
     u64 operator()() { return mt(); }
-    template <typename Int>
-    Int uniformInt(const Int min, const Int max) { return std::uniform_int_distribution<Int>{min, max}(mt); }
-    bool uniformBool() { return static_cast<bool>(uniformInt<int>(0, 1)); }
-    template <typename Real>
-    Real uniformReal(const Real min, const Real max) { return std::uniform_real_distribution<Real>{min, max}(mt); }
-    template <typename Int>
-    std::pair<Int, Int> uniformIntPair(const Int min, const Int max) { return std::minmax(uniformInt(min, max), uniformInt(min, max)); }
-    template <typename Int>
-    std::vector<Int> uniformIntVec(const std::size_t N, const Int min, const Int max)
+    template<typename Int>
+    Int uniform_int(const Int min, const Int max) { return std::uniform_int_distribution<Int>{min, max}(mt); }
+    bool uniform_bool() { return static_cast<bool>(uniform_int<int>(0, 1)); }
+    template<typename Real>
+    Real uniform_real(const Real min, const Real max) { return std::uniform_real_distribution<Real>{min, max}(mt); }
+    template<typename Int>
+    std::pair<Int, Int> uniform_int_pair(const Int min, const Int max) { return std::minmax(uniformInt(min, max), uniformInt(min, max)); }
+    template<typename Int>
+    std::vector<Int> uniform_int_vec(const std::size_t N, const Int min, const Int max)
     {
         std::vector<Int> v(N);
         for (std::size_t i = 0; i < N; i++) { v[i] = uniformInt(min, max); }
         return v;
     }
-    template <typename Real>
-    std::vector<Real> uniformRealVec(const std::size_t N, const Real min, const Real max)
+    template<typename Real>
+    std::vector<Real> uniform_real_vec(const std::size_t N, const Real min, const Real max)
     {
         std::vector<Real> v(N);
         for (std::size_t i = 0; i < N; i++) { v[i] = uniformReal(min, max); }
@@ -33,5 +34,5 @@ public:
 
 private:
     std::mt19937_64 mt;
-} rng;
+} g_rng;
 }  // namespace libtest
