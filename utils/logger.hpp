13d901aee3d6a76814aee92e289fbe5853ed9f81
Pachicobue <tigerssho@gmail.com>
2019-08-24 14:24:44 +0900

    loggerのANSI指定を強化+リファクタリング

--- utils/logger.hpp
+++ utils/logger.hpp
@@ -1,54 +1,20 @@
 #pragma once
 #include <iostream>
 #include <memory>
+
+#include "ansi.hpp"
 namespace libtest {
-enum class Color
-{
-    BLACK,
-    RED,
-    GREEN,
-    YELLOW,
-    BLUE,
-    MAGENTA,
-    CYAN,
-    WHITE,
-    BOLD,
-    RESET,
-};
-class Logger
+class logger
 {
 public:
-    Logger(std::ostream& os) : os{os} {}
-    template <typename T>
-    friend Logger& operator<<(Logger& logger, const T& val) { return logger.silent ? logger : (logger.os << val, logger); }
-    friend Logger& operator<<(Logger& logger, std::ostream& (*f)(std::ostream&)) { return logger.silent ? logger : (logger.os << f, logger); }
-    friend Logger& operator<<(Logger& logger, const Color& color)
+    logger(std::ostream& os) : os{os} {}
+    template<typename T>
+    friend logger& operator<<(logger& logger, const T& val) { return logger.silent ? logger : (logger.os << val, logger); }
+    friend logger& operator<<(logger& logger, std::ostream& (*f)(std::ostream&)) { return logger.silent ? logger : (logger.os << f, logger); }
+    friend logger& operator<<(logger& logger, const ansi_sgr command)
     {
         if (logger.silent) { return logger; }
-        switch (color) {
-        case Color::BLACK:
-            return (logger.os << "\033[30m", logger);
-        case Color::RED:
-            return (logger.os << "\033[31m", logger);
-        case Color::GREEN:
-            return (logger.os << "\033[32m", logger);
-        case Color::YELLOW:
-            return (logger.os << "\033[33m", logger);
-        case Color::BLUE:
-            return (logger.os << "\033[34m", logger);
-        case Color::MAGENTA:
-            return (logger.os << "\033[35m", logger);
-        case Color::CYAN:
-            return (logger.os << "\033[36m", logger);
-        case Color::WHITE:
-            return (logger.os << "\033[37m", logger);
-        case Color::BOLD:
-            return (logger.os << "\033[1m", logger);
-        case Color::RESET:
-            return (logger.os << "\033[0m", logger);
-        default:
-            return logger;
-        }
+        return (logger.os << command.to_string(), logger);
     }
     void mute() { silent = true; }
     void unmute() { silent = false; }
@@ -56,9 +22,9 @@ public:
 private:
     bool silent = false;
     std::ostream& os;
-};
-std::unique_ptr<Logger> g_logger_ptr = std::make_unique<Logger>(std::cout);
-void muteLog() { g_logger_ptr->mute(); }
-void unmuteLog() { g_logger_ptr->unmute(); }
-void redirectLog(std::ostream& os) { g_logger_ptr = std::make_unique<Logger>(os); }
+};  // namespace libtest
+std::unique_ptr<logger> g_logger_ptr = std::make_unique<logger>(std::cout);
+void mute_log() { g_logger_ptr->mute(); }
+void unmute_log() { g_logger_ptr->unmute(); }
+void redirect_log(std::ostream& os) { g_logger_ptr = std::make_unique<logger>(os); }
 }  // namespace libtest
