f0b540dbdb02d9164e05abd32d77b0fd7234e8f5
Pachicobue <tigerssho@gmail.com>
2019-08-26 00:27:03 +0900

    ruqを追加
    dualsegのテスト

--- ruq/problem.hpp
+++ ruq/problem.hpp
@@ -8,12 +8,11 @@ struct ruq
     static constexpr const char* path       = "ruq";
     static constexpr const char* name       = "Range Update Query";
     static constexpr std::size_t time_limit = 2000;
-    using T                                 = ll;
     static void generate_input_small(std::ofstream& input_file)
     {
         constexpr usize n_min = small_constraints::n_min, n_max = small_constraints::n_max;
         constexpr usize q_min = small_constraints::q_min, q_max = small_constraints::q_max;
-        constexpr T v_min = small_constraints::v_min, v_max = small_constraints::v_max;
+        constexpr ll v_min = small_constraints::v_min, v_max = small_constraints::v_max;
         const usize n = g_rng.uniform_int(n_min, n_max);
         const auto vs = g_rng.uniform_int_vec(n, v_min, v_max);
         const usize q = g_rng.uniform_int(q_min, q_max);
@@ -25,9 +24,11 @@ struct ruq
             const usize type = g_rng.uniform_int(0UL, 2UL);
             if (type == 0) {
                 input_file << "0 " << g_rng.uniform_int(0UL, n - 1) << "\n";
+            } else if (type == 1) {
+                input_file << "1 " << g_rng.uniform_int(0UL, n - 1) << " " << g_rng.uniform_int(v_min, v_max) << "\n";
             } else {
                 const auto p = g_rng.uniform_int_pair(0UL, n - 1);
-                input_file << "1 " << p.first << " " << p.second + 1 << " " << g_rng.uniform_int(v_min, v_max) << "\n";
+                input_file << "2 " << p.first << " " << p.second + 1 << " " << g_rng.uniform_int(v_min, v_max) << "\n";
             }
         }
     }
@@ -35,7 +36,7 @@ struct ruq
     {
         constexpr usize n_min = large_constraints::n_min, n_max = large_constraints::n_max;
         constexpr usize q_min = large_constraints::q_min, q_max = large_constraints::q_max;
-        constexpr T v_min = large_constraints::v_min, v_max = large_constraints::v_max;
+        constexpr ll v_min = large_constraints::v_min, v_max = large_constraints::v_max;
         const usize n = g_rng.uniform_int(n_min, n_max);
         const auto vs = g_rng.uniform_int_vec(n, v_min, v_max);
         const usize q = g_rng.uniform_int(q_min, q_max);
@@ -47,9 +48,11 @@ struct ruq
             const usize type = g_rng.uniform_int(0UL, 2UL);
             if (type == 0) {
                 input_file << "0 " << g_rng.uniform_int(0UL, n - 1) << "\n";
+            } else if (type == 1) {
+                input_file << "1 " << g_rng.uniform_int(0UL, n - 1) << " " << g_rng.uniform_int(v_min, v_max) << "\n";
             } else {
                 const auto p = g_rng.uniform_int_pair(0UL, n - 1);
-                input_file << "1 " << p.first << " " << p.second + 1 << " " << g_rng.uniform_int(v_min, v_max) << "\n";
+                input_file << "2 " << p.first << " " << p.second + 1 << " " << g_rng.uniform_int(v_min, v_max) << "\n";
             }
         }
     }
@@ -57,11 +60,11 @@ struct ruq
     {
         usize n;
         input_file >> n;
-        std::vector<T> vs(n);
+        std::vector<ll> vs(n);
         for (auto& e : vs) { input_file >> e; }
         usize q;
         input_file >> q;
-        std::vector<T> ans;
+        std::vector<ll> ans;
         for (usize i = 0; i < q; i++) {
             usize type;
             input_file >> type;
@@ -69,9 +72,14 @@ struct ruq
                 usize i;
                 input_file >> i;
                 ans.push_back(vs[i]);
+            } else if (type == 1) {
+                usize i;
+                ll v;
+                input_file >> i >> v;
+                vs[i] = v;
             } else {
                 usize l, r;
-                T v;
+                ll v;
                 input_file >> l >> r >> v;
                 for (usize i = l; i < r; i++) { vs[i] = v; }
             }
@@ -86,7 +94,7 @@ struct ruq
         solution_output_file >> q_output;
         if (q_actual != q_output) { return false; }
         for (usize i = 0; i < q_actual; i++) {
-            T actual, output;
+            ll actual, output;
             generated_output_file >> actual;
             solution_output_file >> output;
             if (actual != output) { return false; }
@@ -98,13 +106,13 @@ struct ruq
     {
         static constexpr usize n_min = 1, n_max = 100;
         static constexpr usize q_min = 1, q_max = 100;
-        static constexpr int v_min = -100, v_max = 100;
+        static constexpr ll v_min = -100, v_max = 100;
     };
     struct large_constraints
     {
         static constexpr usize n_min = 1, n_max = 100000;
         static constexpr usize q_min = 1, q_max = 100000;
-        static constexpr T v_min = -1000000000, v_max = 1000000000;
+        static constexpr ll v_min = -1000000000, v_max = 1000000000;
     };
 };
 }  // namespace libtest
