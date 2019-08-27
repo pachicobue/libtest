733b95505d0f4163ad23939c72e65990c2f37a33
Pachicobue <tigerssho@gmail.com>
2019-08-27 19:10:30 +0900

    generator用の乱数生成器のリファクタリング

--- utils/random.hpp
+++ utils/random.hpp
@@ -4,35 +4,25 @@
 
 #include "types.hpp"
 namespace libtest {
-class rng
+class random_number_generator
 {
 public:
-    rng() : rng(std::random_device{}()) {}
-    rng(const u64 seed) : mt{seed} {}
-    u64 operator()() { return mt(); }
-    template<typename Int>
-    Int uniform_int(const Int min, const Int max) { return std::uniform_int_distribution<Int>{min, max}(mt); }
-    bool uniform_bool() { return static_cast<bool>(uniform_int<int>(0, 1)); }
-    template<typename Real>
-    Real uniform_real(const Real min, const Real max) { return std::uniform_real_distribution<Real>{min, max}(mt); }
-    template<typename Int>
-    std::pair<Int, Int> uniform_int_pair(const Int min, const Int max, const bool sorted = true) { return sorted ? std::minmax(uniform_int(min, max), uniform_int(min, max)) : std::pair<const Int&, const Int&>{uniform_int(min, max), uniform_int(min, max)}; }
-    template<typename Int>
-    std::vector<Int> uniform_int_vec(const usize N, const Int min, const Int max)
+    random_number_generator() : mt{std::random_device{}()} {}
+    template<typename T>
+    auto gen(const T min, const T max) { return std::uniform_int_distribution<T>{min, max}(mt); }
+    template<typename T>
+    auto gen_vec(const T min, const T max) { return gen(min, max); }
+    template<typename... Args>
+    auto gen_vec(const usize n, Args... args)
     {
-        std::vector<Int> v(N);
-        for (usize i = 0; i < N; i++) { v[i] = uniform_int(min, max); }
-        return v;
+        std::vector<decltype(gen_vec(args...))> ans;
+        for (usize i = 0; i < n; i++) { ans.push_back(gen_vec(args...)); }
+        return ans;
     }
-    template<typename Real>
-    std::vector<Real> uniform_real_vec(const usize N, const Real min, const Real max)
-    {
-        std::vector<Real> v(N);
-        for (usize i = 0; i < N; i++) { v[i] = uniform_real(min, max); }
-        return v;
-    }
+    template<typename T>
+    auto gen_pair(const T min, const T max) { return std::minmax(gen(min, max), gen(min, max)); }
 
 private:
     std::mt19937_64 mt;
-} g_rng;
+} rng;
 }  // namespace libtest
