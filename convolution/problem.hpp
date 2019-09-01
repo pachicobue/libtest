2692348ef17c861f49267e19af5672533e402c8d
Pachicobue <tigerssho@gmail.com>
2019-09-02 01:27:40 +0900

    畳み込み問題

---
+++ convolution/problem.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <fstream>
+
+#include "../utils/printer.hpp"
+#include "../utils/random.hpp"
+#include "../utils/scanner.hpp"
+namespace libtest {
+struct a_minus_b_large
+{
+    static constexpr const char* path       = "convolution";
+    static constexpr const char* name       = "Convolution";
+    static constexpr std::size_t time_limit = 2000;
+    template<typename constraints>
+    static void generate_input(std::ofstream& input_file)
+    {
+        constexpr auto n_min = constraints::n_min, n_max = constraints::n_max;
+        printer pr{input_file};
+        const auto n = rng.gen(n_min, n_max);
+        const auto m = rng.gen(n_min, n_max);
+        pr.println(n, m);
+        const auto a = rng.gen_vec(n, 0, mod - 1);
+        const auto b = rng.gen_vec(m, 0, mod - 1);
+        pr.println(a);
+        pr.println(b);
+    }
+    static void generate_output(std::ifstream& input_file, std::ofstream& output_file)
+    {
+        scanner sc(input_file);
+        printer pr(output_file);
+        const auto [n, m] = sc.read_vals<usize, usize>();
+        const auto a = sc.read_vec<ll>(n), b = sc.read_vec<ll>(m);
+        std::vector<uint> ans(n + m - 1, 0);
+        for (usize i = 0; i < n; i++) {
+            for (usize j = 0; j < m; j++) { (ans[i + j] += a[i] * b[j]) %= mod; }
+        }
+        pr.println(ans);
+    }
+    static bool judge(std::ifstream& input_file, std::ifstream& generated_output_file, std::ifstream& solution_output_file)
+    {
+        scanner in_sc{input_file}, gen_sc(generated_output_file), sol_sc(solution_output_file);
+        const auto [n, m] = in_sc.read_vals<usize, usize>();
+        for (usize i = 0; i < m + n - 1; i++) {
+            if (gen_sc.read<uint>() != sol_sc.may_read<uint>()) { return false; }
+        }
+        return true;
+    }
+    static constexpr uint mod = 924844033;
+    struct small_constraints
+    {
+        static constexpr usize n_min = 1, n_max = 100;
+    };
+    struct large_constraints
+    {
+        static constexpr usize n_min = 1, n_max = 100000;
+    };
+};
+}  // namespace libtest
