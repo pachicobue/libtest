bde64b11c9250cf91e5af329f89ca72feaf0966c
Pachicobue <tigerssho@gmail.com>
2019-09-18 05:32:57 +0900

    RMQ+RAQ+RRQ(区間Rotate)の問題

---
+++ rmq_raq_rrq/problem.hpp
@@ -0,0 +1,118 @@
+#pragma once
+#include <fstream>
+
+#include "../utils/printer.hpp"
+#include "../utils/random.hpp"
+#include "../utils/scanner.hpp"
+namespace libtest {
+struct rmq_raq_rrq
+{
+    static constexpr const char* path       = "rmq_raq_rrq";
+    static constexpr const char* name       = "Range Min Query & Range Add Query & Range Rotate Query";
+    static constexpr std::size_t time_limit = 5000;
+    template<typename constraints>
+    static void generate_input(std::ofstream& input_file)
+    {
+        constexpr auto n_min = constraints::n_min, n_max = constraints::n_max;
+        constexpr auto q_min = constraints::q_min, q_max = constraints::q_max;
+        constexpr auto v_min = constraints::v_min, v_max = constraints::v_max;
+        printer pr{input_file};
+        const auto n = rng.gen(n_min, n_max);
+        const auto q = rng.gen(q_min, q_max);
+        pr.println(n, q);
+        const auto vs = rng.gen_vec(n, v_min, v_max);
+        pr.println(vs);
+        for (usize i = 0; i < q; i++) {
+            const auto type = rng.gen(0UL, 5UL);
+            if (type == 0) {
+                pr.println(type, rng.gen(0UL, n - 1));
+            } else if (type == 1) {
+                pr.println(type, rng.gen(0UL, n - 1), rng.gen(v_min, v_max));
+            } else if (type == 2) {
+                const auto p = rng.gen_pair(0Ul, n - 1);
+                pr.println(type, p.first, p.second + 1);
+            } else if (type == 3) {
+                const auto p = rng.gen_pair(0Ul, n - 1);
+                pr.println(type, p.first, p.second + 1, rng.gen(v_min, v_max));
+            } else if (type == 4) {
+                const auto p = rng.gen_pair(0Ul, n - 1);
+                pr.println(type, p.first, p.second + 1);
+            } else {
+                const auto p = rng.gen_pair(0Ul, n - 1);
+                pr.println(type, p.first, p.second + 1);
+            }
+        }
+    }
+    static void generate_output(std::ifstream& input_file, std::ofstream& output_file)
+    {
+        scanner sc(input_file);
+        printer pr(output_file);
+        const auto [n, q] = sc.read_vals<usize, usize>();
+        auto vs           = sc.read_vec<ll>(n);
+        for (usize i = 0; i < q; i++) {
+            const auto type = sc.read<usize>();
+            if (type == 0) {
+                const auto ind = sc.read<usize>();
+                pr.println(vs[ind]);
+            } else if (type == 1) {
+                const auto [ind, v] = sc.read_vals<usize, ll>();
+                vs[ind]             = v;
+            } else if (type == 2) {
+                const auto [l, r] = sc.read_vals<usize, usize>();
+                pr.println(std::accumulate(vs.begin() + l, vs.begin() + r, std::numeric_limits<ll>::max(), [](const ll a, const ll b) { return std::min(a, b); }));
+            } else if (type == 3) {
+                const auto [l, r, v] = sc.read_vals<usize, usize, ll>();
+                for (usize i = l; i < r; i++) { vs[i] += v; }
+            } else if (type == 4) {
+                const auto [l, r] = sc.read_vals<usize, usize>();
+                const auto h      = vs[l];
+                for (usize i = l; i + 1 < r; i++) { vs[i] = vs[i + 1]; }
+                vs[r - 1] = h;
+            } else {
+                const auto [l, r] = sc.read_vals<usize, usize>();
+                const auto b      = vs[r - 1];
+                for (usize i = r - 1; i >= l + 1; i--) { vs[i] = vs[i - 1]; }
+                vs[l] = b;
+            }
+        }
+    }
+    static bool judge(std::ifstream& input_file, std::ifstream& generated_output_file, std::ifstream& solution_output_file)
+    {
+        scanner in_sc(input_file), gen_sc(generated_output_file), sol_sc(solution_output_file);
+        const auto [n, q] = in_sc.read_vals<usize, usize>();
+        const auto vs     = in_sc.read_vec<ll>(n);
+        for (usize i = 0; i < q; i++) {
+            const auto type = in_sc.read<usize>();
+            if (type == 0) {
+                in_sc.read<usize>();
+                if (gen_sc.read<ll>() != sol_sc.may_read<ll>()) { return false; }
+            } else if (type == 1) {
+                in_sc.read_vals<usize, ll>();
+            } else if (type == 2) {
+                in_sc.read_vals<usize, usize>();
+                if (gen_sc.read<ll>() != sol_sc.may_read<ll>()) { return false; }
+            } else if (type == 3) {
+                in_sc.read_vals<usize, usize, ll>();
+            } else if (type == 4) {
+                in_sc.read_vals<usize, usize>();
+            } else {
+                in_sc.read_vals<usize, usize>();
+            }
+        }
+        return true;
+    }
+
+    struct small_constraints
+    {
+        static constexpr usize n_min = 1, n_max = 100;
+        static constexpr usize q_min = 1, q_max = 100;
+        static constexpr ll v_min = -100, v_max = 100;
+    };
+    struct large_constraints
+    {
+        static constexpr usize n_min = 1, n_max = 100000;
+        static constexpr usize q_min = 1, q_max = 100000;
+        static constexpr ll v_min = -100000, v_max = 100000;
+    };
+};
+}  // namespace libtest
