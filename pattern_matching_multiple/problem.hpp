e1c1f29a3e3e3e46ce8cf0004313cb7e3f03ad95
Pachicobue <tigerssho@gmail.com>
2019-10-01 00:08:28 +0900

    複数パターンの文字列マッチング

---
+++ pattern_matching_multiple/problem.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <fstream>
+
+#include "../utils/printer.hpp"
+#include "../utils/random.hpp"
+#include "../utils/scanner.hpp"
+namespace libtest {
+struct pattern_matching_multiple
+{
+    static constexpr const char* path       = "pattern_matching_multiple";
+    static constexpr const char* name       = "Pattern Matching (Multiple Pattern)";
+    static constexpr std::size_t time_limit = 2000;
+    template<typename constraints>
+    static void generate_input(std::ofstream&) {}
+    static void generate_output(std::ifstream&, std::ofstream&) {}
+    static bool judge(std::ifstream& input_file, std::ifstream& generated_output_file, std::ifstream& solution_output_file)
+    {
+        scanner in_sc{input_file}, gen_sc{generated_output_file}, sol_sc{solution_output_file};
+        const auto [s, n] = in_sc.read_vals<std::string, usize>();
+        for (usize i = 0; i < n; i++) {
+            const auto gen_n = gen_sc.read<usize>();
+            const auto sol_n = sol_sc.may_read<usize>();
+            if (gen_n != sol_n) { return false; }
+            for (usize i = 0; i < gen_n; i++) {
+                if (gen_sc.read<usize>() != sol_sc.may_read<usize>()) { return false; }
+            }
+        }
+        return true;
+    }
+    struct small_constraints
+    {};
+    struct large_constraints
+    {};
+};
+}  // namespace libtest
